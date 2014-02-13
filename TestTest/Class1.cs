c7dfa159c0263b0922db57329d31af0b1d223745
Jakob Florell <jakob.florell@valtech.se>
2014-02-13 10:31:33 +0100

    added some tests again

---
+++ TestTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestTest
+{
+
+    [TestFixture]
+
+    public class Class1
+    {
+        [Test]
+        public void should_be_true()
+        {
+           Assert.True(true); 
+        }
+
+        [Test]
+        public void should_fail()
+        {
+            Assert.True(false);
+        }
+
+    }
+}
